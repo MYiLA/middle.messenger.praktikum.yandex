f2f493b58a5878eb86b12e91008e131f17184f87
fictional_elephant <dyubanova.mb@esphere.ru>
2023-04-16 01:22:06 +0300

    добавлены настройки для ts

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,13 @@ declare module '*.hbs' {
   const template: (param?:any) => string
   export default template
 }
+
+declare module '*.module.scss' {
+  const content: Record<string, string>
+  export default content
+}
+
+declare module '*.svg' {
+  const svg: string
+  export default svg
+}
